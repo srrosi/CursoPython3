170a15bdc9157a13d3e92fe1944c8f0e9550e6ca
Rosivaldo Rodrigues <grr.rodrigues@gmail.com>
2020-10-12 20:09:02 -0400

    Seção 2, aula 17 - Introdução à formatação de Strings.

---
+++ Secao 2/Aula 17.py
@@ -0,0 +1,10 @@
+#Introdução à formatação de Strings em Python
+nome = "Rosivaldo Rodrigues"
+idade = 41
+altura = 1.80
+e_mario = idade > 18
+peso = 80
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', idade, 'anos de idade e seu imc é ', imc)
+print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.2f}')
