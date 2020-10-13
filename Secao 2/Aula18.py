306cf09fe90db6f5c0d396cdd3f650e319985a17
Rosivaldo Rodrigues <grr.rodrigues@gmail.com>
2020-10-12 21:01:40 -0400

    Seção 2, aula 18 - Operadores lógicos - If elif e else.

---
+++ Secao 2/Aula18.py
@@ -0,0 +1,11 @@
+#Input - Entrada de dados do usuário
+
+# nome = input("Qual o seu nome? ")
+# idade = int(input("Idade, por favor? "))
+#
+# print('Olá, {} você tem {} anos de idade'.format(nome, idade))
+
+num1 = int(input('Digite um númermo: '))
+num2 = input('Digite ou número: ')
+
+print(num1 + int(num2))
---
+++ Secao 2/Aula19.py
@@ -0,0 +1,15 @@
+#If, elif, else e booleans
+
+
+# if True:
+#     print("Verdadeiro")
+#     num1 = 2
+#     num2 = 5
+#     print(num2+num2)
+
+if False:
+    print('Verdadeiro')
+elif True:
+    print('Agora sim, é verdadeiro')
+else:
+    print('Não é verdadeiro')
