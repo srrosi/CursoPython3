d910157370f6aa4084ded0cf8f5b1045f3f107a9
Rosivaldo Rodrigues <grr.rodrigues@gmail.com>
2020-10-12 20:21:20 -0400

    Seção 2, aula 17 - Desario prático.

--- Secao 2/Aula15.py
+++ Secao 2/Aula15.py
@@ -8,3 +8,4 @@ imc = peso / (altura ** 2)
 
 print(nome, 'tem', idade, 'anos de idade e seu imc é ', imc)
 print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.2f}')
+print('{} tem {} anos de idade e seu imge é {:.2f} '.format(nome, idade, imc))
---
+++ Secao 2/Aula17.py
@@ -0,0 +1,9 @@
+nome = "Rosivaldo Rodrigues"
+idade = 41
+peso = 80
+altura = 1.80
+anoAtual = 2020
+imc = peso / (altura ** 2)
+
+print('Olá {}, você nasceu em {} e tem {} anos de idade.'.format(nome, (anoAtual - 41), idade))
+print('Olá {}, você pesa {} kilograma e seu IMC é de {:.2f}'.format(nome, peso, imc))
